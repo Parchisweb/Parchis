a41e2110b098e64f9324a67389a39d04c7c78110
Mayank Jha <mayankjha7722@gmail.com>
2019-01-11 20:48:38 +0530

    minor again

--- parchis/src/app/app.module.ts
+++ parchis/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteComponent } from './site/site.component';
+import { BlogComponent } from './site/components/blog/blog.component';
+import { CommentComponent } from './site/components/blog/comment/comment.component';
 
+const appRoutes: Routes = [
+  {path:'', component: SiteComponent},
+  { path: 'site', component: SiteComponent,
+    children: [
+      {path: 'blog', component:BlogComponent},
+      { path: '',
+        redirectTo: './site',
+        pathMatch: 'full'
+      },
+    ]
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent,
+    BlogComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
