ddb0cd6398888d79797e8208f2c4341d59835459
Govind Singh <govind.resarch0000@rediffmail.com>
2019-01-11 22:20:36 +0530

    check this out

--- parchis/src/app/site/site.component.ts
+++ parchis/src/app/site/site.component.ts
@@ -6,15 +6,17 @@ declare var $:any;
   styleUrls: ['./site.component.scss']
 })
 export class SiteComponent implements AfterViewInit {
-
+  is_contact=false;
   constructor() { }
 
   ngAfterViewInit() {
     $.getScript('../assets/js/jquery.min.js');
     $.getScript('../assets/js/main.js');
-    $.getScript('../assets/js/jquery.counterup.min.js');
-    
+    $.getScript('../assets/js/jquery.counterup.min.js'); 
   }
-
+  toggle(){
+    this.is_contact = !this.is_contact;
+    this.ngAfterViewInit();
+  }
 
 }
