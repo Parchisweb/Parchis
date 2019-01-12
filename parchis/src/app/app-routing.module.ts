d88edd4495db533a8cfddafe55ca2f7c4d7c873d
Mayank Jha <mayankjha7722@gmail.com>
2019-01-12 17:12:35 +0530

    Contact finally done!!

--- parchis/src/app/app-routing.module.ts
+++ parchis/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AccountComponent } from './account/account.component';
 import { BlogComponent } from './site/components/blog/blog.component';
 import { DetailsComponent } from './site/components/blog/details/details.component';
-
+import { ContactComponent } from './site/components/contact/contact.component';
 const routes: Routes = [
   {
     path:'admin',
     component:AccountComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
   },
   {
     path:'blog/:id',
